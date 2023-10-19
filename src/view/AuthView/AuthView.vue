4c34b0265922e660ab79e8d514596986c75a52e5
Mikrolab4ik <Mikrolab4ik>
2023-10-19 09:06:44 +0700

    bjn

--- src/view/AuthView/AuthView.vue
+++ src/view/AuthView/AuthView.vue
@@ -1,10 +1,51 @@
 <script setup>
-
-
+import {ref} from "vue"
+const auth = ref(false)
 
 </script>
 
 <template>
+
+    <section>
+
+        <button @click="auth=true">
+            Войти
+        </button>
+        
+        <button @click="auth=false">
+            Зарегестироваться
+        </button>
+    </section>
+    <section v-if="auth">
+        <article>
+            <a>Электронная почта</a>
+            <input type="mail"/>
+            <a>Пароль</a>
+            <input type="password"/>
+            <a>Востанновить доступ</a>
+            <button @click="() => {
+            
+            }">
+                Войти
+            </button>
+        </article>
+
+    </section>
+    <section v-else>
+        <article>
+            <a>Электронная почта</a>
+            <input type="mail"/>
+            <a>Пароль</a>
+            <input type="password"/>
+            <a>Востанновить доступ</a>
+            <button @click="() => {
+            
+            }">
+                Зарегестрироваться
+            </button>
+        </article>
+
+    </section>
 
 
 </template>
