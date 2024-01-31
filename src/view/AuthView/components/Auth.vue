f883abb1a9597af20f3188c0fe4bde4ba4ec70ac
Nitromer234 <ura.klimov1@gmail.com>
2024-01-31 20:52:15 +0700

    жопа

--- src/view/AuthView/components/Auth.vue
+++ src/view/AuthView/components/Auth.vue
@@ -4,13 +4,82 @@
 
 <template>
     <section>
-        <h5>Вход</h5>
-        <p>Электронная почта</p>
-        <input type="mail"/>
-        <p>Пароль</p>
-        <input type="password"/>
-        <p><v-btn variant="outlined">Войти</v-btn></p>
-        <p>У вас нет аккаунта?<button @click="$router.push('/auth/reg')">Зарегистрироваться</button></p>
+        <h5 class="vhod">Вход</h5>
+        <p class="text">Электронная почта или телефон</p>
+        
+        <v-responsive
+        class="baseinput">
+        <v-text-field 
+        label="email@mail.com"
+        type="email"></v-text-field>
+        </v-responsive>
+
+        <p class="text">Пароль</p>
+        <v-responsive
+        class="baseinput">
+        <v-text-field
+        type="password"></v-text-field>
+        </v-responsive>
+
+        <p class="font"><v-btn color="#75A3CF" class="button">
+        Войти
+        </v-btn></p>
+        <p class="font">У вас нет аккаунта?<button @click="$router.push('/auth/reg')"><p class="next">Зарегистрироваться</p></button></p>
     </section>
 </template>
 
+<style>
+.font{
+    font-family: Gilroy;
+}
+.baseinput{
+    border-radius: 2px;
+    border: 1px solid #E53835;
+    background: var(--Red-R50, #FFFAFA);
+    display: flex;
+    width: 446px;
+    height: 42px;
+    padding: 0px 12px 0px 10px;
+    justify-content: center;
+    align-items: center;
+    flex-shrink: 0;
+}
+
+.vhod{
+    color: var(--Text-primary---gray700, #313131);
+text-align: center;
+font-family: Gilroy;
+font-size: 22px;
+font-style: normal;
+font-weight: 500;
+line-height: normal;
+}
+.text{
+    color: var(--Text-primary---gray700, #313131);
+font-family: Gilroy;
+font-size: 14px;
+font-style: normal;
+font-weight: 400;
+line-height: 16px; /* 114.286% */
+justify-content: center;
+}
+.button{
+    display: flex;
+width: 446px;
+padding: 0px 22px;
+flex-shrink: 0;
+justify-content: center;
+align-items: center;
+gap: 10px;
+border-radius: 2px;
+background: #75A3CF;
+}
+.next{
+    color: #75A3CF;
+font-family: Gilroy;
+font-size: 18px;
+font-style: normal;
+font-weight: 500;
+line-height: 150%;
+}
+</style>
--- src/view/AuthView/components/Reg.vue
+++ src/view/AuthView/components/Reg.vue
@@ -1,39 +1,76 @@
 <script setup>
 
 
-
 </script>
 
 <template>
     
     <section>
-        <h5>Регистрация</h5>
-        <p>Ваше ФИО в полной форме</p>
-        <input/>
-        <p>Электронная почта</p>
-        <input type="mail"/>
-        <p>Контактный телефон</p>
-        <input/>
-        <p>Название компании или ИНН</p>
-        <input/>
-        <p>Придумайте Пароль</p>
-        <input type="password"/>
-        <p>Подтвердите Пароль</p>
-        <input type="password"/>
-        <p><v-btn>Зарегистрироваться</v-btn></p>
-        <p>Уже есть аккаунт? <button @click="$router.push('/auth/auth')">Войти</button></p>
+        
+      <h5 class="reg">Регистрация</h5>
+      
+        <p class="text">Ваше ФИО в полной форме</p>
+        <v-responsive class="baseinput"><v-text-field></v-text-field></v-responsive>
+        <p class="text">Электронная почта</p>
+        <v-responsive class="baseinput">
+        <v-text-field 
+        label="email@mail.com"
+        type="email"></v-text-field></v-responsive>
+        <p class="text">Контактный телефон</p>
+        <v-responsive class="baseinput"><v-text-field></v-text-field></v-responsive>
+        <p class="text">Название компании или ИНН</p>
+        <v-responsive class="baseinput"><v-text-field></v-text-field></v-responsive>
+        <p class="text">Придумайте Пароль</p>
+        <v-responsive class="baseinput" ><v-text-field type="password"
+          ></v-text-field></v-responsive>
+        <p class="text">Подтвердите Пароль</p>
+        <v-responsive class="baseinput"><v-text-field type ="password"></v-text-field></v-responsive>
+        <p class="text"><v-col cols="auto">
+        <p><v-btn color="#75A3CF" class="button">Зарегистрироваться</v-btn></p>
+      </v-col></p>
+        <p>Уже есть аккаунт? <button @click="$router.push('/auth/auth')"><p class="next">Войти</p></button></p>
+      
     </section>
 
 
 </template>
 
 <style scoped>
-.button {
-  border: none;
+.baseinput{
+  width: 446px;
+  height: 42px;
+  flex-shrink: 0;
   border-radius: 2px;
+  border: 1px solid var(--Gray-gray150, #E1E1E1);
+  background: var(--Gray-White, #FFF);
 }
-
-.button-close {
-  background-color: red;
+.text{
+  color: var(--Text-primary---gray700, #313131);
+  font-family: Gilroy;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 16px; /* 114.286% */
+  Top: 277px;
+  Left: 284px
+}
+.reg{
+  color: var(--Text-primary---gray700, #313131);
+  text-align: center;
+  font-family: Gilroy;
+  font-size: 22px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  top: 176px;
+  left: 439px;
+}
+.next{
+  color: #75A3CF;
+  font-family: Gilroy;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 150%;
 }
 </style>
